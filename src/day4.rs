36d863e21eed946bd4f0abe04b8b35cfa739873a
Min <minsun@stuffofminsun.com>
2025-01-09 16:47:53 -0500

    fixed day 4 and 5

--- src/day4.rs
+++ src/day4.rs
@@ -1,15 +1,15 @@
-use std::fs;
+use std::{env, fs};
 extern crate md5;
 
 fn part_one(contents: &String) -> i32 {
     let mut counter = 0;
-    let result = loop {
+    let _result = loop {
         let checksum = format!("{contents}{}", counter.to_string());
         let digest = md5::compute(checksum);
         let val = format!("{:x}", digest);
         if &val[..5] == "00000" {
-            println!("match {} {}", counter, val);
-            break counter
+            // println!("match {} {}", counter, val);
+            break counter;
         }
         counter += 1;
     };
@@ -18,13 +18,13 @@ fn part_one(contents: &String) -> i32 {
 
 fn part_two(contents: &String) -> i32 {
     let mut counter = 0;
-    let result = loop {
+    let _result = loop {
         let checksum = format!("{contents}{}", counter.to_string());
         let digest = md5::compute(checksum);
         let val = format!("{:x}", digest);
         if &val[..6] == "000000" {
-            println!("match {} {}", counter, val);
-            break counter
+            // println!("match {} {}", counter, val);
+            break counter;
         }
         counter += 1;
     };
@@ -32,15 +32,13 @@ fn part_two(contents: &String) -> i32 {
 }
 
 fn main() {
-    let contents = fs::read_to_string("data/day4.txt")
-        .expect("Should have been able to read the file");
-
-
+    let home = env::current_dir().expect("The dir should be here");
+    let filepath = home.join("src/data/day4.txt");
+    let contents = fs::read_to_string(filepath).expect("Should have been able to read the file");
 
     let result = part_one(&contents);
     println!("Part 1: {result}");
 
     let result = part_two(&contents);
     println!("Part 2: {result}");
-
 }
