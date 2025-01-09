15e41f410eead8c5fc8cfa404fd3bc68dd3bfb50
Min <minsun@stuffofminsun.com>
2025-01-09 16:44:02 -0500

    fix: updated day2

--- src/day2.rs
+++ src/day2.rs
@@ -5,15 +5,14 @@ fn part_one(contents: &Vec<String>) -> i32 {
     let mut wrapping_paper_area: i32 = 0;
 
     for c in contents {
-        if c.chars().count() == 0 { println!("End"); }
-        else {
+        if c.chars().count() == 0 {
+        } else {
             let a: Vec<i32> = c.split('x').map(|s| s.parse().unwrap()).collect();
             if let [l, w, h] = &a[0..3] {
-                let areas = [l*w, w*h, h*l];
-                wrapping_paper_area += (2*l*w) + (2*w*h) + (2*h*l);
+                let areas = [l * w, w * h, h * l];
+                wrapping_paper_area += (2 * l * w) + (2 * w * h) + (2 * h * l);
                 wrapping_paper_area += areas.iter().min().unwrap()
-            }
-            else {
+            } else {
                 println!("Wrong shaped vector");
             };
         }
@@ -25,7 +24,8 @@ fn part_two(contents: &Vec<String>) -> i32 {
     let mut ribbon: i32 = 0;
 
     for c in contents {
-        if c.chars().count() == 0 { println!("End"); } else {
+        if c.chars().count() == 0 {
+        } else {
             let a: Vec<i32> = c.split('x').map(|s| s.parse().unwrap()).collect();
             if let [l, w, h] = &a[0..3] {
                 let areas = [2 * (l + w), 2 * (w + h), 2 * (h + l)];
@@ -40,13 +40,10 @@ fn part_two(contents: &Vec<String>) -> i32 {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let file_path = &args[1];
-    println!("Using {file_path}");
-
     // I guess the split() returns &str
-    let contents: Vec<String> = fs::read_to_string(file_path)
+    let home = env::current_dir().expect("Hmm");
+    let filepath = home.join("src/data/day2.txt");
+    let contents: Vec<String> = fs::read_to_string(filepath)
         .expect("Should have been able to read the file")
         .split("\n")
         .map(|s| s.to_string())
@@ -57,5 +54,4 @@ fn main() {
 
     let result = part_two(&contents);
     println!("Part 2: {result}");
-
 }
